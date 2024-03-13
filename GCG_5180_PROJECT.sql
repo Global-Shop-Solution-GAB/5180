993839112276f42e28f6a13520387fd21cffaaa6
Service Web <sw@gssmail.com>
2024-03-13 12:24:34 -0500

    Commit changes in C:\Websites\Git\emarlow\5180\GCG_5180_PROJECT.sql

---
+++ GCG_5180_PROJECT.sql
@@ -0,0 +1,10 @@
+/* ==========================================
+ * TABLE: GCG_5180_Project
+ * ========================================== */
+CREATE TABLE "GCG_5180_Project"(
+ "Identity" IDENTITY DEFAULT '0',
+ "Terminal" CHAR(50),
+ "Project" CHAR(50),
+ UNIQUE ("Identity"));
+
+
